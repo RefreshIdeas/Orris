c76cde0aed7bcb929b1aae47815d4fe359ffebf1
Ashish <agfrontedndeveloper@gmail.com>
2023-09-22 18:58:22 +0530

    latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+plugins:[],
+
+
+// extends
+    extend: {
+
+// font size start
+      fontSize: {
+        
+        'heroTitle': ['8rem', {
+          lineHeight: '8rem',
+          fontWeight: '500',
+        }],
+
+        'heroTitleSub': ['4.2rem', {
+          lineHeight: '8rem',
+          fontWeight: '400',
+        }],
+      },
+
+      // font fontFamily start
+      fontFamily: {
+        'centabel': ['centabel', ...defaultTheme.fontFamily.sans],
+      },
+
+      
+
+    },
   },
-  plugins: [],
+
 }
