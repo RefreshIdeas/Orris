91de8be0515dfb6de3a0c5a64c6d1584db25319f
Ashish <agfrontedndeveloper@gmail.com>
2023-09-25 12:55:27 +0530

    latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ plugins:[],
 // extends
     extend: {
 
+
+      dropShadow: {
+theme1:'0 4px 4px rgba(0, 0, 0, 0.25)',
+
+      },
+
 // font size start
       fontSize: {
         
@@ -19,9 +25,29 @@ plugins:[],
         }],
 
         'heroTitleSub': ['4.2rem', {
-          lineHeight: '8rem',
+          lineHeight: ' rem',
           fontWeight: '400',
         }],
+        'sectionHeading': ['4.2rem', {
+          lineHeight: '5rem',
+          fontWeight: '500',
+        }],
+
+        'sectionSubHeading': ['2rem', {
+          lineHeight: '3.4rem',
+          fontWeight: '300',
+        }],
+      },
+
+
+      colors:{
+        theme1:'#007A7F',
+        theme2:'#3A3947',
+        light_blue:'#F5F8FF',
+        light_blue:'#D2AF6F',
+        themeGradient1:'#FFB400',
+        themeGradient1_1:'#B87A02'
+         
       },
 
       // font fontFamily start
