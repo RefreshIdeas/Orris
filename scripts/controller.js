8fffbd227aec83ff44d328f1e6ef8114d384245c
Ashish <agfrontedndeveloper@gmail.com>
2023-10-21 13:08:01 +0530

    WFH Share

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,10 +1,8 @@
-import "owl.carousel/dist/assets/owl.carousel.css"; // Owl Carousel CSS
+
 import "owl.carousel"; // Owl Carousel JavaScript
 import "./events";
 import VenoBox from "venobox";
 import "venobox/dist/venobox.min.css";
-
-
 
 $(".white_Arrow_Vcenter").owlCarousel({
   loop: true,
@@ -84,7 +82,7 @@ $(".theme_dot_Hleft").owlCarousel({
   responsive: {
     0: {
       items: 1,
-     
+
       // mouseDrag: true,
       // touchDrag: true,
     },
@@ -92,24 +90,25 @@ $(".theme_dot_Hleft").owlCarousel({
       items: 1,
       mouseDrag: true,
       touchDrag: true,
-     
     },
     1000: {
       items: 1,
-
-     
     },
   },
 });
 
 $(".owl-prev").html(
-  `<svg xmlns="http://www.w3.org/2000/svg"  width="100" height="100" viewBox="0 0 20 20"><path fill="currentColor" d="M12.353 15.854a.5.5 0 0 1-.707.001L6.162 10.39a.55.55 0 0 1 0-.78l5.484-5.464a.5.5 0 1 1 .706.708L7.188 10l5.164 5.147a.5.5 0 0 1 .001.707Z"/></svg>`
+  `<svg width="22" height="40" viewBox="0 0 22 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18.6367 0.474155L0.496906 18.7939C0.178308 19.1156 0 19.5472 0 19.9966C0 20.4459 0.178308 20.8775 0.496906 21.1993L18.6367 39.5258C18.7851 39.6759 18.9626 39.7952 19.1586 39.8766C19.3546 39.958 19.5652 40 19.7779 40C19.9907 40 20.2012 39.958 20.3972 39.8766C20.5932 39.7952 20.7707 39.6759 20.9191 39.5258C21.2244 39.2181 21.3953 38.805 21.3953 38.3748C21.3953 37.9447 21.2244 37.5316 20.9191 37.2238L3.86093 19.9966L20.9191 2.77273C21.2234 2.46516 21.3936 2.05278 21.3936 1.62344C21.3936 1.1941 21.2234 0.78173 20.9191 0.474155C20.7707 0.324098 20.5932 0.204831 20.3972 0.123394C20.2012 0.0419568 19.9907 0 19.7779 0C19.5652 0 19.3546 0.0419568 19.1586 0.123394C18.9626 0.204831 18.7851 0.324098 18.6367 0.474155Z" fill="#3A3947"/>
+</svg>`
 );
 $(".owl-next").html(
-  `<svg xmlns="http://www.w3.org/2000/svg"  width="100" height="100" viewBox="0 0 20 20"><path fill="currentColor" d="M7.646 4.147a.5.5 0 0 1 .707-.001l5.484 5.465a.55.55 0 0 1 0 .779l-5.484 5.465a.5.5 0 0 1-.706-.708L12.812 10L7.647 4.854a.5.5 0 0 1-.001-.707Z"/></svg>`
+  `<svg width="22" height="40" viewBox="0 0 22 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M3.36327 0.474155L21.5031 18.7939C21.8217 19.1156 22 19.5472 22 19.9966C22 20.4459 21.8217 20.8775 21.5031 21.1993L3.36327 39.5258C3.21486 39.6759 3.03742 39.7952 2.84142 39.8766C2.64542 39.958 2.43483 40 2.22209 40C2.00935 40 1.79876 39.958 1.60276 39.8766C1.40676 39.7952 1.22932 39.6759 1.08091 39.5258C0.775585 39.2181 0.60465 38.805 0.60465 38.3748C0.60465 37.9447 0.775585 37.5316 1.08091 37.2238L18.1391 19.9966L1.08091 2.77273C0.776632 2.46516 0.606359 2.05278 0.606359 1.62344C0.606359 1.1941 0.776632 0.78173 1.08091 0.474155C1.22932 0.324098 1.40676 0.204831 1.60276 0.123394C1.79876 0.0419568 2.00935 0 2.22209 0C2.43483 0 2.64542 0.0419568 2.84142 0.123394C3.03742 0.204831 3.21486 0.324098 3.36327 0.474155Z" fill="#3A3947"/>
+  </svg>`
 );
 
-const intserSectingElement = document.querySelectorAll('section');
+const intserSectingElement = document.querySelectorAll("section");
 let observer = new IntersectionObserver(
   (val) => {
     if (val[0].isIntersecting) {
@@ -121,7 +120,7 @@ let observer = new IntersectionObserver(
 intserSectingElement.forEach((val) => observer.observe(val));
 // counter initfunction
 function counterInit(param) {
-const speed = 200;
+  const speed = 200;
   const counters = document.querySelectorAll(".counterUP");
 
   counters.forEach((counter) => {
@@ -146,97 +145,76 @@ const speed = 200;
   });
 }
 
-
-
 // toggle function globally
 function addToogleEvent() {
-let click = document.querySelectorAll('.clickEvent')
-click.forEach((val) => {val.addEventListener('click',toggleFunc)})
-};
+  let click = document.querySelectorAll(".clickEvent");
+  click.forEach((val) => {
+    val.addEventListener("click", toggleFunc);
+  });
+}
 
 function toggleFunc(e) {
+  let nameof_class_tobe_toggle =
+    e.target.closest(".clickEvent").dataset.toggleto;
+  if (!nameof_class_tobe_toggle) return;
+  let classes_tobe_toggle = document.querySelectorAll(
+    `.${nameof_class_tobe_toggle}`
+  );
+  console.log(classes_tobe_toggle);
 
-let nameof_class_tobe_toggle = e.target.closest('.clickEvent').dataset.toggleto;
-if(!nameof_class_tobe_toggle) return;
-let classes_tobe_toggle = document.querySelectorAll(`.${nameof_class_tobe_toggle}`);
-console.log(classes_tobe_toggle)
-
-Array.from(classes_tobe_toggle).forEach(element =>{
-  if(element.classList.contains('navigationSection')) document.querySelector('html').classList.toggle('navActive')
-   element.classList.toggle('active');
+  Array.from(classes_tobe_toggle).forEach((element) => {
+    if (element.classList.contains("navigationSection"))
+      document.querySelector("html").classList.toggle("navActive");
+    element.classList.toggle("active");
   });
 }
 addToogleEvent();
 
-
 /// nav events
-let mainNav = document.querySelectorAll('.nav');
-let parentDiv_subnav = document.querySelector('.subNav_block');
-let allSubNavs = document.querySelectorAll('.subNavs_list');
+let mainNav = document.querySelectorAll(".nav");
+let parentDiv_subnav = document.querySelector(".subNav_block");
+let allSubNavs = document.querySelectorAll(".subNavs_list");
 
-mainNav.forEach(element =>{
-  element.addEventListener('click', function(e){
-    let target = e.target.closest('.nav');
+mainNav.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    let target = e.target.closest(".nav");
     let SubNavName = target.dataset.main_nav_of;
     let subnav = document.querySelector(`.${SubNavName}`);
-    allSubNavs.forEach(element => element.classList.remove('active'));
-    mainNav.forEach(element => element !== target && element.classList.remove('active'));
-    
-    if(!parentDiv_subnav.classList.contains('active')){
-      parentDiv_subnav.classList.add('active')
+    allSubNavs.forEach((element) => element.classList.remove("active"));
+    mainNav.forEach(
+      (element) => element !== target && element.classList.remove("active")
+    );
+
+    if (!parentDiv_subnav.classList.contains("active")) {
+      parentDiv_subnav.classList.add("active");
     }
-       if(target.classList.contains('active')){
-        target.classList.remove('active');
-        parentDiv_subnav.classList.remove('active')
+    if (target.classList.contains("active")) {
+      target.classList.remove("active");
+      parentDiv_subnav.classList.remove("active");
+    } else {
+      target.classList.add("active");
+    }
 
-       }
-       else{
-        target.classList.add('active');
-
-       }
-  
     let targetPostionTop = target.getBoundingClientRect().top;
-    if(screen.width > 640){
-      parentDiv_subnav.style=`margin-top: ${targetPostionTop}px`;
-
+    if (screen.width > 640) {
+      parentDiv_subnav.style = `margin-top: ${targetPostionTop}px`;
     }
-    subnav.classList.toggle('active');
+    subnav.classList.toggle("active");
 
-    if(!SubNavName) return;
-  })
-})
+    if (!SubNavName) return;
+  });
+});
 
-
-let subNavBlock = document.querySelectorAll('.subNav_block');
-subNavBlock.forEach(element=>{
-  element.addEventListener('click', function(e){
-    mainNav.forEach(element => element.classList.remove('active'));
-
-  })
-
-  
-})
-
-
-
-
-
-
-
-
-
-
-
-
+let subNavBlock = document.querySelectorAll(".subNav_block");
+subNavBlock.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    mainNav.forEach((element) => element.classList.remove("active"));
+  });
+});
 
 // veno box for pop image and video
 new VenoBox({
   selctor: ".venobox",
 });
 
-
-
-
-
 // /trash
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,100 +1,63 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
-const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require("tailwindcss/plugin");
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-plugins:[],
+    plugins: [],
 
-
-// extends
+    // extends
     extend: {
-
-      screens: {
-       
-      },
+      screens: {},
 
       padding: {
-        sectionPadding: '75px',
-        
-        sectionPaddingMOB: '75px',
-        sectionPaddingTAB: '75px',
+        sectionPadding: "55px",
+        sectionPaddingMOB: "75px",
+        sectionPaddingTAB: "75px",
       },
 
       margin: {
-        sectionmargin: '75px',
-      
+        sectionmargin: "75px",
       },
-
 
       dropShadow: {
-        primary_shadow:'0 4px 4px rgba(0, 0, 0, 0.25)',
-        secondary_shadow:'10px 10px 25px 0px rgba(0, 0, 0, 0.10)',
-
-
+        primary_shadow: "0 4px 4px rgba(0, 0, 0, 0.25)",
+        secondary_shadow: "10px 10px 25px 0px rgba(0, 0, 0, 0.10)",
       },
 
-
-     
-
-// font size start
-      // fontSize: {
-        
-      
-
-      //   'heroTitleSub': ['4.2rem', {
-      //     lineHeight: '4rem',
-      //     fontWeight: '400',
-      //   }],
-      //   'sectionHeading': ['4.2rem', {
-      //     lineHeight: '5rem',
-      //     fontWeight: '500',
-      //   }],
-
-      //   'sectionSubHeading': ['2rem', {
-      //     lineHeight: '3.4rem',
-      //     fontWeight: '300',
-      //   }],
-      //   'sectionText': ['1.8rem', {
-      //     lineHeight: '3rem',
-      //     fontWeight: '300',
-      //   }],
-
-
-      // },
-
-
-      colors:{
-        
-        primary_clr:'#007A7F',
-        secondary_clr:'#3A3947',
+      colors: {
+        primary_clr: "#007A7F",
+        secondary_clr: "#3A3947",
 
         // bg colors
-        primary_BG_clr:'#F5F8FF',        
-        secondary_BG_clr:'#F5F4F4',  
-        tertiary_BG_clr:'#FFB400',
-        quartary_BG_clr:'#B87A02',
-        senary_BG_clr:'#D2AF6F',
-        septenary_BG_clr:'#5B5B5B',
+        primary_BG_clr: "#F5F8FF",
+        secondary_BG_clr: "#F5F4F4",
+        tertiary_BG_clr: "#FFB400",
+        quartary_BG_clr: "#B87A02",
+        senary_BG_clr: "#D2AF6F",
+        septenary_BG_clr: "#5B5B5B",
 
         // border colors
-        primary_border_clr:'#D9D9D9',
-        secondary_border_clr:'rgba(#fff,0.25)',     
-        
+        primary_border_clr: "#D9D9D9",
+        secondary_border_clr: "rgba(#fff,0.25)",
 
         // borderClrs
-       
-         
       },
 
+      container: {
+        center: true,
+        screens: {
+          sm: "600px",
+          md: "728px",
+          lg: "984px",
+          xl: "1240px",
+          "2xl": "1240px",
+        },
+      },
       // font fontFamily start
       fontFamily: {
-        'centabel': ['centabel', ...defaultTheme.fontFamily.sans],
+        centabel: ["centabel", ...defaultTheme.fontFamily.sans],
       },
-
-      
-
     },
   },
-
-}
+};
